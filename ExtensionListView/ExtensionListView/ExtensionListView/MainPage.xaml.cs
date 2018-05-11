09a829d6cbbb02a287053478b088a86e96c9510c
Ashton Yoon <y109384@naver.com>
2018-05-11 11:18:30 +0900

    create project

---
+++ ExtensionListView/ExtensionListView/ExtensionListView/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExtensionListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Task.Run(() =>
+            {
+                var source = Enumerable.Range(0, 1000).Select(x => x.ToString()).ToArray();
+
+                var templatedSource = new List<IGrouping<string, string>>(
+                    source.OrderBy(x => x).GroupBy(x => x[0].ToString().ToUpper()));
+
+                return templatedSource;
+            }).ContinueWith(task =>
+            {
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    listView.ItemsSource = task.Result;
+                });
+            });
+        }
+    }
+}
